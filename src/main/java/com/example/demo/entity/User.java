cebbe6361f8cb3b5078e8a4816cd75d52752c265
madhan <madhandadi001@gmail.comn>
2024-07-10 12:24:16 -0500

    Jwt Authentication

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,23 +1,32 @@
 package main.java.com.example.demo.entity;
 
 import jakarta.persistence.*;
+import main.java.com.example.demo.utils.PasswordUtils;
 
 @Entity
 @Table(name = "user")
 public class User {
+
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long UserID;
-    private String Name;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userid")
+    private Long userId;
+    private String name;
+    private String email;
+    private String password;
 
-    public long getUserID() { return  UserID;}
+    public long getUserID() { return  userId;}
     public String getName() {
-        return Name;
+        return name;
     }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
 
+    public void setUserID(long userId) { this.userId = userId;}
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
-
-    public void setUserID(long userID) { UserID = userID;}
+    public void setEmail(String email) {this.email = email; }
+    public void setPassword(String password) {this.password = PasswordUtils.encodePassword(password)    ; }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,6 +2,7 @@ package main.java.com.example.demo.service;
 
 import main.java.com.example.demo.entity.User;
 import main.java.com.example.demo.respository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,7 +10,9 @@ import java.util.Optional;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
