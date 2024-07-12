2e86e858471afa892eda254419d93ff6d2cc7730
madhan <madhandadi001@gmail.comn>
2024-07-12 12:31:59 -0500

    added log4j logger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package main.java.com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final Logger logger = LogManager.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
 
+		SpringApplication.run(DemoApplication.class, args);
+
+		logger.info("Application Started Successfully on port 8080.");
+		logger.debug("This is a debug message.");
+		logger.error("This is an error message.");
+	}
 }
